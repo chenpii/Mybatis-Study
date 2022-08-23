2db4ea24376f67b825c4218372c60d99d604abf6
chenp <641760512@qq.com>
2022-08-23 19:33:13 +0800

    日志工厂-log4j日志分级

--- mybatis-04/src/test/java/com/kuang/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/com/kuang/dao/UserMapperTest.java
@@ -3,6 +3,7 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 
@@ -11,11 +12,13 @@ import org.junit.Test;
  * @create 2022-08-21 11:47
  */
 public class UserMapperTest {
+    static Logger logger = Logger.getLogger(UserMapperTest.class);
 
     @Test
     public void test_getUserById() {
-
         SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        logger.info("测试，进入了test_getUserById方法成功！");
 
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         User user = mapper.getUserById(1);
@@ -25,6 +28,13 @@ public class UserMapperTest {
         //关闭SqlSession
         sqlSession.close();
 
+    }
+
+    @Test
+    public void testLog4j() {
+        logger.info("info:进入了testLog4j方法");
+        logger.debug("debug:进入了testLog4j方法");
+        logger.error("error:进入了testLog4j方法");
     }
 
 }
