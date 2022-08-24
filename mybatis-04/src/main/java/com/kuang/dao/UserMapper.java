44de98d1f43f343922972d8074523e03cb1b8c89
chenp <641760512@qq.com>
2022-08-24 08:49:46 +0800

    limit分页

--- mybatis-04/src/main/java/com/kuang/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/kuang/dao/UserMapper.java
@@ -2,6 +2,9 @@ package com.kuang.dao;
 
 import com.kuang.pojo.User;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author chenpi
  * @create 2022-08-21 11:35
@@ -11,4 +14,6 @@ public interface UserMapper {
     //根据ID查用户
     User getUserById(int id);
 
+    //分页
+    List<User> getUserLimit(Map<String, Integer> map);
 }
--- mybatis-04/src/test/java/com/kuang/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/com/kuang/dao/UserMapperTest.java
@@ -6,6 +6,10 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * @author chenpi
@@ -15,7 +19,23 @@ public class UserMapperTest {
     static Logger logger = Logger.getLogger(UserMapperTest.class);
 
     @Test
-    public void test_getUserById() {
+    public void getUserLimit() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("startIndex", 0);
+        map.put("pageSize", 2);
+        List<User> userList = mapper.getUserLimit(map);
+        for (User user : userList) {
+            logger.info(user);
+        }
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void getUserById() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         logger.info("测试，进入了test_getUserById方法成功！");
