0bef25c8a56a0e9ddfc55cd1426146a475c3efd0
chenp <641760512@qq.com>
2022-08-25 19:37:46 +0800

    log4j只在04中引用

--- mybatis-05/src/test/java/com/kuang/dao/UserMapperTest.java
+++ mybatis-05/src/test/java/com/kuang/dao/UserMapperTest.java
@@ -3,7 +3,6 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
@@ -11,7 +10,6 @@ import org.junit.Test;
  * @create 2022-08-21 11:47
  */
 public class UserMapperTest {
-    static Logger logger = Logger.getLogger(UserMapperTest.class);
 
     @Test
     public void getUsers() {
@@ -19,7 +17,7 @@ public class UserMapperTest {
 
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         for (User user : mapper.getUsers()) {
-            logger.info(user);
+            System.out.println(user);
         }
 
         sqlSession.close();
