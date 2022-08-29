74175e53dfbeefbbe812f5d9601e401669ed8650
chenp <641760512@qq.com>
2022-08-29 11:16:20 +0800

    lombok注解

--- mybatis-05/src/main/java/com/kuang/pojo/User.java
+++ mybatis-05/src/main/java/com/kuang/pojo/User.java
@@ -1,53 +1,18 @@
 package com.kuang.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author chenpi
  * @create 2022-08-21 11:33
  */
+@Data //无参、get、set、tostring、equals、hashcode
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int id;
     private String name;
     private String password;
-
-    public User() {
-    }
-
-    public User(int id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
