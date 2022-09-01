d803b62979a7aa7a6105b25a8e27c0e6b36374ba
chenp <641760512@qq.com>
2022-09-01 14:39:03 +0800

    mybatis-09 二级缓存

--- mybatis-09/src/main/java/com/kuang/pojo/User.java
+++ mybatis-09/src/main/java/com/kuang/pojo/User.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable{
+    private static final long serialVersionUID = -6849794885475667710L;
+
     private int id;
     private String name;
     private String pwd;
--- mybatis-09/src/test/java/com/kuang/dao/TestUserMapper.java
+++ mybatis-09/src/test/java/com/kuang/dao/TestUserMapper.java
@@ -5,8 +5,6 @@ import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class TestUserMapper {
@@ -18,10 +16,10 @@ public class TestUserMapper {
         User user = mapper.queryUserByID(1);
         System.out.println(user);
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("id", 1);
-        map.put("name", "狂神2");
-        map.put("pwd", "888888");
+        //Map<String, Object> map = new HashMap<>();
+        //map.put("id", 1);
+        //map.put("name", "狂神2");
+        //map.put("pwd", "888888");
         //mapper.updateUser(map);
 
         sqlSession.clearCache();//手动清理一级缓存
@@ -29,10 +27,30 @@ public class TestUserMapper {
         System.out.println("==================================================");
         User user2 = mapper.queryUserByID(1);
         System.out.println(user2);
-
         System.out.println(user == user2);
 
         sqlSession.close();
+
+    }
+
+    @Test
+    public void test_getUsers2() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        SqlSession sqlSession2 = MybatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        UserMapper mapper2 = sqlSession2.getMapper(UserMapper.class);
+
+        User user = mapper.queryUserByID(1);
+        System.out.println(user);
+
+        sqlSession.close();
+        System.out.println("==================================================");
+        User user2 = mapper2.queryUserByID(1);
+        System.out.println(user2);
+        System.out.println(user == user2);
+
+        sqlSession2.close();
 
     }
 }
